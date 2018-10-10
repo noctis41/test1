f74bfc35ec61b98760c1e5f55f23067b1b2b0bc8
noctis41 <43877138+noctis41@users.noreply.github.com>
2018-10-10 14:26:33 +0200

    Create traitement.php

---
+++ traitement.php
@@ -0,0 +1,23 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+<title>Document sans titre</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<meta http-equiv="refresh" content="0; URL=formulaire.html">
+</head>
+<?php
+$prenom = $_POST['Prenom'];
+$nom = $_POST['Nom'];
+ 
+$message = "$prenom $nom";
+ 
+$to = 'jolybenjamin@voila.fr';
+$objet = "Test" //Met l'objet que tu veux
+ 
+/* En-têtes de l'e-mail */
+$headers = "From: $nom \r\n\r\n";
+  
+/* Envoi de l'e-mail */
+mail($to, $objet, $message, $headers)
+ 
+?>
